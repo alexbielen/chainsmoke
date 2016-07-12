2023882a0831140c02502207b992f8421bf5f4dd
Alex Bielen <bielenah@gmail.com>
2016-07-11 22:31:52 -0400

    Travis config change so that it runs the pytest tests.

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 
 setup(name='chainsmoke',
       version='0.1',
@@ -9,8 +23,6 @@ setup(name='chainsmoke',
       license='MIT',
       packages=['chainsmoke'],
       install_requires=[
-            'pytest',
+          'pytest',
       ],
-      zip_safe=False
       )
-
