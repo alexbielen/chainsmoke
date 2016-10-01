2cc28af19e5e78e4caa91dc1b70e9e7f3a50733c
Alex Bielen <alexhendriebielen@gmail.com>
2016-10-01 14:59:20 -0400

    Tests for decision tree.

--- chainsmoke/decide.py
+++ chainsmoke/decide.py
@@ -25,6 +25,17 @@ from typing import Any, Callable, Union
 Result = namedtuple('Result', ['value', 'path'])
 
 
+class Decision:
+    pass
+
+
+class Action:
+    pass
+
+
+DecisionType = Union[Decision, Action]
+
+
 class Decision(object):
     """
     Represents a binary choice node.
@@ -33,16 +44,22 @@ class Decision(object):
     def __init__(self, name: str, predicate: Callable[[Any], bool], false_next: DecisionType, true_next: DecisionType):
         """
         :param name: Name of the decision
-        :param predicate: A 1-arity function that returns
-        :param false_next:
-        :param true_next:
+        :param predicate: A 1-arity function that returns a boolean
+        :param false_next: A DecisionType object that is returned when predicate is False
+        :param true_next: A DecisionType object that is returned when predicate is True
         """
         self.name = name
         self.predicate = predicate
         self.false_next = false_next
         self.true_next = true_next
 
-    def next(self, data, path=''):
+    def next(self, data: Any, path: str = ''):
+        """
+        Calls the next DecisionType in the chain.
+        :param data: Any value
+        :param path: A string the represents the path taken through the tree
+        :return: A DecisionType object
+        """
         if not path:
             path = self.name + " -> "
         else:
@@ -57,13 +74,18 @@ class Decision(object):
 
 
 class Action(object):
+    """
+    Represents a leaf node in the decision tree.
+    """
+
     def __init__(self, name: str, action: Callable[[Any], Any]):
+        """
+        :param name: Name of the action
+        :param action: A 1-arity callable
+        """
         self.name = name
         self.action = action
 
     def next(self, data: Any, path: str) -> Result:
         result = self.action(data)
         return Result(value=result, path=path + self.name)
-
-
-DecisionType = Union[Decision, Action]
--- tests/test_decision.py
+++ tests/test_decision.py
@@ -26,7 +26,54 @@ SOFTWARE.
 from chainsmoke.decide import Decision, Action
 
 
+class DriversLicense(object):
+    def __init__(self, age, name, state, valid):
+        self.age = age
+        self.name = name
+        self.state = state
+        self.valid = valid
 
 
+# test data
+alex_drivers_license = DriversLicense(29, 'Alex', 'Ohio', False)
+denis_drivers_license = DriversLicense(22, 'Denis', 'New York', True)
+dave_drivers_license = DriversLicense(20, 'Dave', 'Maryland', True)
 
 
+# helper functions
+def check_out_of_state(driver_license):
+    if driver_license.state != 'New York':
+        return driver_license.valid
+    else:
+        return True
+
+
+# decision tree
+do_not_allow_in_bar = Action('do not allow in bar', lambda x: 'Get out of the bar, {name}'.format(name=x.name))
+allow_into_bar = Action('allow in bar', lambda x: 'Welcome to the bar, {name}'.format(name=x.name))
+check_that_out_of_state_id_is_valid = Decision(
+    'check that out of state id is valid',
+    check_out_of_state,
+    false_next=do_not_allow_in_bar,
+    true_next=allow_into_bar
+)
+check_over_21 = Decision(
+    'check that person is over 21',
+    lambda x: x.age >= 21,
+    false_next=do_not_allow_in_bar,
+    true_next=check_that_out_of_state_id_is_valid
+)
+
+
+def test_that_decision_tree_behaves_properly():
+    alex_result = check_over_21.next(alex_drivers_license)
+    dave_result = check_over_21.next(dave_drivers_license)
+    denis_result = check_over_21.next(denis_drivers_license)
+
+    assert alex_result.value == 'Get out of the bar, Alex'
+    assert dave_result.value == 'Get out of the bar, Dave'
+    assert denis_result.value == 'Welcome to the bar, Denis'
+
+    assert alex_result.path == 'check that person is over 21 -> check that out of state id is valid -> do not allow in bar'
+    assert dave_result.path == 'check that person is over 21 -> do not allow in bar'
+    assert denis_result.path == 'check that person is over 21 -> check that out of state id is valid -> allow in bar'
